aaa204311a31061e96b68672a08e47e8fbf84294
deepakdravid555-hub <deepakdravid555@gmail.com>
2026-07-25 12:10:17 +0530

    Update index.js
    Implemented temperature conversion from Celsius to Fahrenheit using map() function.

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-// Temperature Converter using map()
-
 const celsius = [0, 20, 30, 40];
-
-// TODO:
-// Convert the Celsius temperatures to Fahrenheit
-// Formula: (C × 9/5) + 32
-// Use the map() function and store the result in fahrenheit.
-
-let fahrenheit = [];
-
-// Write your code below
-
-
-// Do not modify this line
+let fahrenheit = celsius.map(temp=>(temp*9/5)+32);
+console.log(fahrenheit)
 module.exports = fahrenheit;
